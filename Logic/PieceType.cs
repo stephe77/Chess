a92a2df318d5fb94f279440c306c9a4d4b72dd97
stephe77 <stefnlv@mail.ru>
2023-12-28 03:43:07 +0300

    added rules for pieces

---
+++ Logic/PieceType.cs
@@ -0,0 +1,12 @@
+﻿namespace Logic
+{
+    public enum PieceType
+    {
+        Pawn, //пешка
+        Bishop, //слон
+        Knight, //конь
+        Rook, //ладья
+        Queen, //королева
+        King //король
+    }
+}
---
+++ Logic/Pieces/Piece.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public abstract class Piece
+    {
+        public abstract PieceType Type { get; }
+        public abstract Player Color { get; }
+        public bool HasMoved { get; set; } = false;
+
+        public abstract Piece Copy();
+
+        public abstract IEnumerable<Move> GetMoves(Position from, Board board);
+
+        //ладья, королева и ферзь. шаги у них в несколько идут, добавляется "ограничение" для понимания возможных ходов
+        protected IEnumerable<Position> MovePositionsInDir(Position from, Board board, Direction dir)
+        {
+            for (Position pos = from + dir; Board.IsInside(pos); pos+=dir)
+            {
+                if (board.IsEmpty(pos))
+                {
+                    yield return pos;
+                    continue;
+                }
+
+                Piece piece = board[pos];
+
+                if (piece.Color != Color)
+                {
+                    yield return pos;
+                }
+
+                yield break;
+            }
+        }
+
+        protected IEnumerable<Position> MovePositionsInDirs(Position from, Board board, Direction[] dirs)
+        {
+            return dirs.SelectMany(dir => MovePositionsInDir(from, board, dir));
+        }
+    }
+}
